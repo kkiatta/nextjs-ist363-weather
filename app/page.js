7fcd1cda7326c405e50800b8f4e97637189475ea
kkiatta <kkiattaw@syr.edu>
2024-04-09 18:25:21 -0400

    attendance 4/9/24 dayforecast

--- app/page.js
+++ app/page.js
@@ -8,26 +8,70 @@ import Button from "../components/Button";
 import ButtonDemo from "@/components/ButtonDemo";
 import ColorPicker from "../components/ColorPicker";
 import PeoplePicker from "@/components/PeoplePickers";
-import { getPeople, getWeatherData} from "../lib/api"
+import { getPeople, getWeatherData, getGeoLocation, getWeatherDataByLatLon} from "../lib/api"
 
 
 const Homepage = () => {
   const [weatherData, setWeatherData] = useState (null);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [daysOfWeek, setDaysofWeek] = useState(null);
+  const [activeDayIndex, setActiveDayIndex] = useState(0)
 
   const peopleArr = getPeople();
 
+  useEffect(()=>{
+    getGeoLocation()
+    .then((position) => {
+      console.log(position);
+      setLocation(position);
+    })
+      .catch((error) => {
+        setErrorMsg(error);
+      });
+  },[]);
+
   useEffect(() => {
     const fetchData = async () => {
-      const response = await getWeatherData();
-      setWeatherData(response);
-    };
-    fetchData();
-  }, []); //feed two thing 1. arrow function, array of dependecy that trigger it
+        const response = await getWeatherDataByLatLon(location);
+        setWeatherData(response);
+      };
+      location ? fetchData(): null;
+  },[location]);
+
+    useEffect(() => {
+      //filter out the days of the week
+      const tempWeek  = [];
+
+      weatherData && 
+        weatherData.list.filter((block) => {
+          const date = new Date(block.dt * 1000); //handle date form weatherapp
+          const options = { weekday: "short"}; //hold structure set
+          const day = date.toLocaleDateString("en-US", options); //localize date format 
+         // console.log(day);
+         if (!tempWeek.includes()) {// ! mean if you do not have date inside we
+            tempWeek.push(day);
+         }
+      });
+
+      setDaysofWeek(tempWeek);
+      // then set state with the days of the wekk
+    }, [weatherData]);
+
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await getWeatherData();
+  //     setWeatherData(response);
+  //   };
+  //   fetchData();
+  // }, []); //feed two thing 1. arrow function, array of dependecy that trigger it
   
   //console.log({peopleArr}); 
   return (
   <div>
     <h1>Weather app</h1>
+    {errorMsg && <div>{errorMsg}</div>}
       {weatherData && (
         <div>
          <h2>{weatherData.city.name}</h2>
@@ -44,6 +88,27 @@ const Homepage = () => {
       {/*<PeoplePicker people={peopleArr}/>
       <ButtonDemo />
   <ColorPicker />*/}
+  {daysOfWeek && (
+  <section>
+      <ul>
+      {daysOfWeek?.map((day, index) => {
+        return <li key={index}>{day}</li>;
+      })}
+      </ul>
+      <div>
+        {weatherData?.list
+        .filter((block) => {
+        const date = new Date(block.dt * 1000);
+        const options = { weekdat: "short"};
+        const day = date.toLocaleDateString("en-US", options);
+        return day === daysOfWeek[activeDayIndex];
+      })
+      .map((block,index) => {
+        return <p key={index}>{block.main.temp}</p>;
+      })}
+      </div>
+  </section>
+    )}
   </div>
   );
 };
